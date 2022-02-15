d783a8e401e0f953433760393206bb90f35be921
unknown <difabiomauricio@gmail.com>
2022-02-15 14:48:07 -0300

    JS7 terminado

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,26 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var letra = nombre.charAt(0).toUpperCase();
+  var resto = nombre.substring(1,nombre.length);
+  return letra+resto;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  
+    return cb();
+  
+
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Vamos a recibir una función que realiza una operación matemática como callback junto
+  // con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,19 +31,29 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let suma = numeros.reduce(function(acu,elemento){
+    return acu+elemento;
+  });
+return cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  let arr = array.forEach(function(elemento){return cb(elemento);});
+  
 }
 
 function map(array, cb) {
   // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto 
+  //por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+    
+    let Nuevo=array.map(function (elemento){return cb(elemento)})
+  return Nuevo;
 }
 
 function filter(array) {
